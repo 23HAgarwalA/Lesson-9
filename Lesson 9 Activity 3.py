c9d3baab1f7e4d769a028d9ff320210f8ebc6dcc
23HAgarwalA <23HAgarwalA@qerdp.co.uk>
2026-03-15 15:25:53 +0000

    initial commit

---
+++ Lesson 9 Activity 3.py
@@ -0,0 +1,24 @@
+print ("Select your ride: ")
+print ("1. Bike")
+print ("2. Car")
+choice = int(input("Enter your choice: "))
+if (choice == 1):
+    print ("What type of bike?")
+    print ("1. Scooty\n")
+    print ("2. Scooter\n")
+    choice2=int(input("Enter your choice: "))
+    if choice2 == 1:
+        print ("You have selected the scooty")
+    else: 
+        print ("You have selected the scooter")
+elif (choice == 2):
+    print ("What type of car?")
+    print ("1. Sedan")
+    print ("2. XUV")
+    choice3=int(input("Enter your choice3: "))
+    if choice3==1:
+        print ("You have selected sedan")
+    else:
+        print ("You have selected XUV")
+else: 
+    print ("Wrong choice!")
