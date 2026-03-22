b3abe38706c4c4dfc2420d2d76bf47514928d5aa
23HAgarwalA <23HAgarwalA@qerdp.co.uk>
2026-03-22 14:56:16 +0000

    initial commit

---
+++ Lesson 9 HW 1.py
@@ -0,0 +1,5 @@
+age = int(input("Please enter your age: "))
+if age < 20 and age > 10:
+    print ("You are between 10 and 20 years old")
+else:
+    print ("You are not between 10 and 20 years old")
